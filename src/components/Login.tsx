112b6c8d60050a27c0fe5c658dfd85b70cdb3d22
Hugo <>
2021-02-23 16:00:16 -0500

    login popup

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+function Login() {
+    return (
+        <form className="login form-signin ">
+            <label htmlFor="inputUsername" className="sr-only">Username</label>
+            <input type="username" id="inputUsername" className="form-control" placeholder="Username" required />
+            <label htmlFor="inputPassword" className="sr-only">Password</label>
+            <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+            <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    )
+}
+
+export default Login
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+import Login from './Login';
+
 const Nav = () => {
+
+    const [showLogin, setShowLogin] = useState(false)
+    const loginClicked = () => {
+        setShowLogin(!showLogin)
+    }
     return (
-        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <>
+            <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
                 <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
                 <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -8,10 +17,15 @@ const Nav = () => {
                 <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
                 <ul className="navbar-nav px-3">
                     <li className="nav-item text-nowrap">
-                        <a className="nav-link" href="#">Sign out</a>
+                        <button className="form-control-dark" onClick={loginClicked}>
+                            Log in
+                            </button>
                     </li>
                 </ul>
+
             </nav>
+            {showLogin && <Login />}
+        </>
     )
 }
 
